d7412913d4489e662b32d8514cdecf9da76a6a42
Calvin Cheng <calvin@calvinx.com>
2012-05-07 16:27:24 +0800

    Add in setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='Python Cherrypy Wsgiserver',
+    version='0.1dev',
+    packages=['wsgiserver', ],
+    license='LICENSE',
+    description='Wsgiserver configured for cherrypy and django',
+    long_description=open('README.md').read(),
+    author='Calvin Cheng',
+    author_email='calvin@calvinx.com',
+    install_requires=[],
+)
